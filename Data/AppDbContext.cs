b19870e6c19123749159ccadee0dce587d73448a
Ranyel Cerracena de Sousa <rcerracena@gmail.com>
2024-10-23 11:15:09 -0300

    Incluido a webapi ao github

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos => Set<Todo>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("DataSource=app.db");
+    }
+}
---
+++ Endpoints/TodoEndpoints.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Data;
+using MinimalApi.Models;
+
+namespace MinimalApi.Endpoints
+{
+    public static class TodoEndpoints
+    {
+        public static void MapTodoEndpoints(this IEndpointRouteBuilder routes)
+        {
+            routes.MapGet("/todos", async (AppDbContext context) =>
+            {
+                var todos = await context.Todos.ToListAsync();
+                return Results.Ok(todos);
+            });
+
+            routes.MapPost("/todos", async (AppDbContext context, Todo todo) =>
+            {
+                context.Todos.Add(todo);
+                await context.SaveChangesAsync();
+                return Results.Created($"todos/{todo.Id}", todo);
+            });
+
+            routes.MapPut("/todos/{id}", async (AppDbContext context, int id, Todo todoInput) =>
+            {
+                var todo = await context.Todos.FindAsync(id);
+                if (todo is null) return Results.NotFound();
+
+                todo.Title = todoInput.Title;
+                todo.IsCompleted = todoInput.IsCompleted;
+
+                await context.SaveChangesAsync();
+                return Results.Ok(todo);
+            });
+
+            routes.MapDelete("/todos/{id}", async (AppDbContext context, int id) =>
+            {
+                var todo = await context.Todos.FindAsync(id);
+                if (todo is null) return Results.NotFound();
+
+                context.Todos.Remove(todo);
+                await context.SaveChangesAsync();
+                return Results.NoContent();
+            });
+        }
+    }
+}
